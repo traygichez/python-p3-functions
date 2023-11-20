64c59a68b43210cdccaaf3951cfe4c0e9d1fa39c
tray <onesimus.githua@student.moringaschool.com>
2023-11-21 01:13:08 +0300

    completed lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,42 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+
+greet("John")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+
+greet_with_default()  
+greet_with_default("John")
+
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+result = add(5, 3)
+print(result) 
+
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+
+    return number / 2
+
+
+result = halve(8)
+print(result)  
+
+result = halve("string")
+print(result)  
+
